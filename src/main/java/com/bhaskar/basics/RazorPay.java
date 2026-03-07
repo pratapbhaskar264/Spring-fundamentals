a39bd13d9ec9dfce11995a247f015ea3caf3cc34
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-07 22:50:19 +0530

    RuntimeComponent-configurtion

--- src/main/java/com/bhaskar/basics/RazorPay.java
+++ src/main/java/com/bhaskar/basics/RazorPay.java
@@ -1,8 +1,12 @@
 package com.bhaskar.basics;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "payment.provider" , havingValue = "RazorPay" )
 public class RazorPay implements PaymentGateway{
 
     @Override
--- src/main/java/com/bhaskar/basics/StripePay.java
+++ src/main/java/com/bhaskar/basics/StripePay.java
@@ -1,9 +1,11 @@
 package com.bhaskar.basics;
 
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
+@ConditionalOnProperty(name = "payment.provider" , havingValue = "StripePay" )
 public class StripePay implements PaymentGateway{
     @Override
     public void pay() {
