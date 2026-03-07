60214906b8a7bd8f7669a455358722491708a9c8
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-07 21:24:15 +0530

    looseCoupling

--- src/main/java/com/bhaskar/basics/BasicsApplication.java
+++ src/main/java/com/bhaskar/basics/BasicsApplication.java
@@ -1,14 +1,25 @@
 package com.bhaskar.basics;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class BasicsApplication {
+public class BasicsApplication implements CommandLineRunner {
+
+	@Autowired // loose coupling
+    private greet greet1;
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicsApplication.class, args);
-		System.out.println("hey");
+		ApplicationContext context =
+				SpringApplication.run(BasicsApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		greet1.Greet();
+	}
 }
---
+++ src/main/java/com/bhaskar/basics/greet.java
@@ -0,0 +1,11 @@
+package com.bhaskar.basics;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class greet {
+    public void Greet() {
+        System.out.println("Good afternoon boii");
+    }
+
+}
