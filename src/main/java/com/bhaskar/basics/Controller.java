1a8fc542103daef6a98f208c59984d334abcdaed
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-18 14:14:51 +0530

    Logger

--- src/main/java/com/bhaskar/basics/Controller.java
+++ src/main/java/com/bhaskar/basics/Controller.java
@@ -1,16 +1,32 @@
 package com.bhaskar.basics;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+//import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("v1")
 public class Controller {
 
+   private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    public int divide(int a , int  b ) throws Exception {
+        try{
+            int result = a / b;
+            return result;
+        } catch(Exception e) {
+            logger.error("0 nhi beta");
+            return 0;
+        }
+    }
     @GetMapping("/greet")
-    public String fn() {
+    public String fn()  throws Exception {
+        System.out.println(divide(10,0));
         return "HEY GOOD  AFTERNOON";
     }
 }
--- src/main/java/com/bhaskar/basics/greet.java
+++ src/main/java/com/bhaskar/basics/greet.java
@@ -1,10 +1,15 @@
 package com.bhaskar.basics;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @Component
 public class greet {
-    public void Greet() {
+
+
+    public void Greet(){
         System.out.println("Good afternoon boii");
     }
 
